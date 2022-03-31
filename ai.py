79216f1baded86bae63906e9915caae97dda60c0
Tony Greener <tony.greener@me.com>
2022-03-31 11:51:09 -0700

    Created AI Class

---
+++ ai.py
@@ -0,0 +1,9 @@
+from player import Player
+import random
+from time import sleep
+
+class AI(Player):
+    def __init__(self, name):
+        super().__init__(name)
+
+    
--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 class Player():
     def __init__(self, name):
         self.name = name
+        self.score = 0
         self.gesture_list = ['Rock', 'Paper','Scissor', 'Lizard', 'Spock']
