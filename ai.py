b81bdf02066efd44f4b8fba8203a0865e8636373
Tony Greener <tony.greener@me.com>
2022-03-31 12:06:03 -0700

    Choose Gesture Method Added to AI Class

--- ai.py
+++ ai.py
@@ -6,4 +6,6 @@ class AI(Player):
     def __init__(self, name):
         super().__init__(name)
 
-    
+    def choose_gesture(self):
+        self.choose_gesture = random.randint(0,4)
+        print(f'{self.name} has picked {self.gesture_list[self.choose_gesture]}')
