41d679ff37a8d89f1e49eb3d6e28ecd6bf298f29
Tony Greener <tony.greener@me.com>
2022-03-31 12:38:46 -0700

    Added Choose Gesture Method to Human Class

--- human.py
+++ human.py
@@ -4,3 +4,13 @@ from time import sleep
 class Human(Player):
     def __init__(self, name):
         super().__init__(name)
+
+    def choose_gesture(self):
+        i = 0
+        for gesture in self.gesture_list:
+            print(f'Press {i} to select {gesture}')
+            i += 1
+            sleep(1)
+        self.choose_gesture = input
+        sleep(1)
+        print(f'{self.name} has picked {self.gesture_list[self.choose_gesture]}')
